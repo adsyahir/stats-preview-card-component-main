2027f7c41c9bfaeac1376eff5f629fe48405919e
adsyahir <adsyahir16@gmail.com>
2021-08-10 18:19:58 +0800

    adam

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     <div className="container">
+       <div className="box-1 padding bg-color-1 order-2">
+         <h1>Get <span> insights </span> that help your business grow. </h1>
+         <p class="p">Discover the benefits of data analytics and mnake better decisions
+         regarding revenue, customer experience, and overall efficiency.</p>
+        <div class="flex justify-content">
+        <div class="col">
+        <h2>10k+</h2>
+        <p>COMPANIES</p>
+        </div>
+        <div class="col">
+        <h2>314</h2>
+        <p>TEMPLATES</p>
+        </div>
+        <div class="col">
+        <h2>12M+</h2>
+        <p>QUERIES</p>
+        </div>
+        </div>
+       </div>
+       <div className=" img order-1 ">
+        <div class="bg-color">
+        <div class="overlay-2">
+
+        </div>
+        </div>
+       </div>
+  
+     </div>
+</div>
+    
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return ( 
+        <footer>
+<p>
+        Challenge by
+        <a href="https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62">Frontend Mentor</a>.
+      </p>
+      
+      <p>
+        Coded by
+        <a href="https://github.com/adsyahir">Adam Syahir</a>.
+      </p>
+        </footer>
+        );
+}
+ 
+export default Footer;
